05c83f005097727f3da8f0fc39ab74f920740e5e
Tom White <tom@sixdozen.com>
2013-01-16 00:13:51 -0800

    victory: sample blade program working downstream from strokes
    First project with strokes as a dependency working. Currently
    just a copy of the blade demo program, but about to start morphing.

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject victory "0.0.1-SNAPSHOT"
+  :description "victory: experimental thinger"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [net.drib/strokes "0.0.1-SNAPSHOT"]]
+  :min-lein-version "2.0.0"
+  :source-paths ["src/clj" "src/cljs"]
+
+  :plugins [[lein-cljsbuild "0.2.10"]]
+
+  :cljsbuild {
+    :builds [{
+      :source-path "src/cljs"
+      :compiler {
+        :output-to "public/out/victory.js"
+        :optimizations :whitespace
+        :pretty-print true 
+        ; :optimizations :simple
+        }}]})
---
+++ test/victory/core_test.clj
@@ -0,0 +1,7 @@
+(ns victory.core-test
+  (:use clojure.test
+        victory.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
