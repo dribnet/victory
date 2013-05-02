d93645b5465a11735f8b335de0a01520a4724be9
Tom White <tom@sixdozen.com>
2013-05-02 00:01:28 -0700

    added upsample method to resizer

--- render/resizer/src/resizer/core.clj
+++ render/resizer/src/resizer/core.clj
@@ -9,13 +9,13 @@
 (defn write-file [im s]
   (try
     (ImageIO/write im "png" (File. s))
-    (catch IOException e (str "caught exception: " (.getMessage e)))
+    (catch IOException e (str "file - " s " - caught exception: " (.getMessage e)))
     ))
 
 (def prefix "../tiles/")
 
 ; resize a file like ../18/0/0.png to ../17/0/0.png
-(defn resize [zoom x y]
+(defn down-sample [zoom x y]
   (let [im (BufferedImage. 1024 1024 BufferedImage/TYPE_INT_RGB)
         g  (.createGraphics im)
         him (BufferedImage. 512 512 BufferedImage/TYPE_INT_RGB)
@@ -44,15 +44,57 @@
     ; (write-file qim "tile.png")
     ))
 
+; resize a file like ../18/0/0.png to ../19/[01]/[01].png
+(defn up-sample [zoom x y]
+  (let [im (BufferedImage. 512 512 BufferedImage/TYPE_INT_RGB)
+        g  (.createGraphics im)
+        him (BufferedImage. 256 256 BufferedImage/TYPE_INT_RGB)
+        hg (.createGraphics him)
+        infile (str prefix zoom "/" x "/" y ".png")
+        nzoom (inc zoom)]
+    (.setRenderingHint hg RenderingHints/KEY_INTERPOLATION
+                       RenderingHints/VALUE_INTERPOLATION_NEAREST_NEIGHBOR)
+    (println (str "reading " infile))
+    (.drawImage g (ImageIO/read (File. infile))
+      0 0 512 512 nil)
+
+    ; make two images at zoom+1, x*2
+    (jio/make-parents (str prefix nzoom "/" (* x 2) "/out.png"))
+    (.drawImage hg im 0 0 256 256 0 0 256 256 nil)
+    (write-file him (str prefix nzoom "/" (* x 2) "/" (* y 2) ".png"))
+    (.drawImage hg im 0 0 256 256 0 256 256 512 nil)
+    (write-file him (str prefix nzoom "/" (* x 2) "/" (inc (* y 2)) ".png"))
+
+    ; make two images at zoom+1, x*2+1
+    (jio/make-parents (str prefix nzoom "/" (inc (* x 2)) "/out.png"))
+    (.drawImage hg im 0 0 256 256 256 0 512 256 nil)
+    (write-file him (str prefix nzoom "/" (inc (* x 2)) "/" (* y 2) ".png"))
+    (.drawImage hg im 0 0 256 256 256 256 512 512 nil)
+    (write-file him (str prefix nzoom "/" (inc (* x 2)) "/" (inc (* y 2)) ".png"))
+    ))
+
+; (doseq [i (range -1 4)]
+;   (doseq [j (range -1 4)]
+;     (up-sample 60 i j)))
+; (doseq [i (range -2 8)]
+;   (doseq [j (range -2 8)]
+;     (up-sample 61 i j)))
+; (doseq [i (range -4 16)]
+;   (doseq [j (range -4 16)]
+;     (up-sample 62 i j)))
+; (doseq [i (range -8 32)]
+;   (doseq [j (range -8 32)]
+;     (up-sample 63 i j)))
+
 ; (doseq [i (range -18 18 2)]
 ;   (doseq [j (range -18 18 2)]
-;     (resize 18 i j)))
+;     (down-sample 18 i j)))
 ; (doseq [i (range -8 8 2)]
 ;   (doseq [j (range -8 8 2)]
-;     (resize 17 i j)))
+;     (down-sample 17 i j)))
 ; (doseq [i (range -2 2 2)]
 ;   (doseq [j (range -2 2 2)]
-;     (resize 16 i j)))
+;     (down-sample 16 i j)))
 
 
 (defn -main
